6acf35d96b5aa0c1f6e13bf799a9b37ed4ae3c85
ChinDowns <webdev@chindowns.com>
2020-04-14 18:43:28 -0700

    Working with a TOC

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# readme-generator
+# readme-generator <hr/>  
+    https://img.shields.io/date/1586822400
+## Description
+Command Line Interface README.md generator   
+
+Table of Contents
+## [title](#title)
+Table of Contents
+## [description](#description)
+Table of Contents
+## [installation](#installation)
+Table of Contents
+## [usage](#usage)
+Table of Contents
+## [license](#license)
+Table of Contents
+## [contribute](#contribute)
+Table of Contents
+## [tests](#tests)
+Table of Contents
+## [author](#author)
+Table of Contents
+## [github](#github)
+Table of Contents
+## [githubUrl](#githubUrl)
+## Installation
+Install instructions here   
+
+## Usage
+readme-gen.js   
+
+## Licenses
+MIT   
+
+## Authors   
+![Site](https://github.com/chindowns.png)  Michael Downs
+https://github.com/chindowns
--- readme-gen.js
+++ readme-gen.js
@@ -1,5 +1,9 @@
-var FileIO = require('./lib/fileIO');
-// var fileIO = new FileIO();
+const FileIO = require('./lib/fileIO');
+const inquirer = require('inquirer');
+const questions = require('./lib/askQuestions');
+const fileIO = new FileIO();
+const ReadmeGen = require('./lib/generateMarkdown.js')
+let path = "./README.md";
 
 function init() {
     console.log(`This utility will walk you through creating a README.md file.
@@ -8,11 +12,56 @@ function init() {
     Press ^C at any time to quit.`);
 
     // Check if Readme exists
-    if (FileIO.exists('./README.md')) {
-        console.log('README.md file already exists, please delete and run again if you want to generate a new file');
+    if (fileIO.exists('./README.md')) {
+        return console.log('README.md file already exists, please delete and run again if you want to generate a new file');
     }
 
+    inquirer.prompt(questions)
+    .then(answers => {
+        const readmeGen = new ReadmeGen();
 
+        readmeGen.title(path, answers.title);
+        readmeGen.shieldVersion(path);
+        
+        if (answers.description !== "") {
+        readmeGen.section(path, "Description", answers.description);
+        }
+
+        // Create Table of Contents based on answers that are not empty
+        let toc = [];
+        let count = 0;
+        Object.keys(answers).forEach (answer => {
+            if (answer !== "") {
+                readmeGen.toc(path, answer, count);
+                count++;
+        }});
+        console.log(toc);
+        
+        if (answers.install !== "") {
+            readmeGen.section(path, "Installation", answers.installation);
+        }
+
+        if (answers.usage !== "") {
+            readmeGen.section(path, "Usage", answers.usage);
+        }
+
+        if (answers.license !== "") {
+            readmeGen.section(path, "Licenses", answers.license);
+        }
+
+        if(answers.contribute !== "") {
+            readmeGen.section(path, "Contributers", answers.contribute);
+        }
+
+        if(answers.tests !== "") {
+            readmeGen.section(path, "Tests", answers.tests);
+        }
+
+        if(answers.author !== "") {
+            readmeGen.author(path, answers.author, answers.githubUrl);
+        }
+        console.log("README.md is complete");
+    });
 
 }
 
